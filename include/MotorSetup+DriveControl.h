0c0d4b414983b3d1fe9805d22ac57ff388914569
dacen <wach.dacen25@svvsd.org>
2020-01-23 15:13:08 -0700

    make proper vex structure and added all files

---
+++ include/MotorSetup+DriveControl.h
@@ -0,0 +1,108 @@
+#include "vex.h"
+using namespace vex;
+
+//----------------------------------------Motor Setup----------------------------------------//
+brain         Brain;
+motor         frontRight(PORT12, gearSetting::ratio18_1, true);
+motor         frontLeft(PORT11, gearSetting::ratio18_1);
+motor         backRight(PORT3, gearSetting::ratio18_1, true);
+motor         backLeft(PORT4, gearSetting::ratio18_1);
+motor         leftIntake(PORT7, gearSetting::ratio18_1);
+motor         rightIntake(PORT9, gearSetting::ratio18_1, true);
+motor         ramp(PORT5, true);
+motor         lift(PORT6, true);
+gyro          gyroSandwhich(Brain.ThreeWirePort.A);
+controller    Controller1;
+controller    Controller2(controllerType::partner);
+int Switch = 0;
+void forwards( float rotations, int vel ){
+
+frontRight.startRotateFor(rotations,rotationUnits::rev,vel,vex::velocityUnits::pct);
+frontLeft.startRotateFor(rotations,rotationUnits::rev,vel,vex::velocityUnits::pct);
+backRight.startRotateFor(rotations,rotationUnits::rev,vel,vex::velocityUnits::pct);
+backLeft.rotateFor(rotations,rotationUnits::rev,vel,vex::velocityUnits::pct);
+}
+
+void forwardsWhile( float rotations, int vel ){
+
+frontRight.startRotateTo(rotations,rotationUnits::rev,vel,vex::velocityUnits::pct);
+frontLeft.startRotateTo(rotations,rotationUnits::rev,vel,vex::velocityUnits::pct);
+backRight.startRotateTo(rotations,rotationUnits::rev,vel,vex::velocityUnits::pct);
+backLeft.startRotateTo(rotations,rotationUnits::rev,vel,vex::velocityUnits::pct);
+}
+
+void backward( float rotations, int vel ){
+
+frontRight.startRotateFor(-rotations,rev,vel,vex::velocityUnits::pct);
+frontLeft.startRotateFor(-rotations,rev,vel,vex::velocityUnits::pct);
+backRight.startRotateFor(-rotations,rev,vel,vex::velocityUnits::pct);
+backLeft.rotateFor(-rotations,rev,vel,vex::velocityUnits::pct);
+}
+
+void righto( float rotations, int vel ){
+
+frontRight.startRotateFor(-rotations,rotationUnits::rev,vel,vex::velocityUnits::pct);
+frontLeft.startRotateFor(rotations,rotationUnits::rev,vel,vex::velocityUnits::pct);
+backRight.startRotateFor(-rotations,rotationUnits::rev,vel,vex::velocityUnits::pct);
+backLeft.rotateFor(rotations,rotationUnits::rev,vel,vex::velocityUnits::pct);
+}
+
+void lefto(float rotations,int vel){
+
+frontRight.startRotateFor(rotations,rotationUnits::rev,vel,vex::velocityUnits::pct);
+frontLeft.startRotateFor(-rotations,rotationUnits::rev,vel,vex::velocityUnits::pct);
+backRight.startRotateFor(rotations,rotationUnits::rev,vel,vex::velocityUnits::pct);
+backLeft.rotateFor(-rotations,rotationUnits::rev,vel,vex::velocityUnits::pct);
+}
+
+
+void intake(float Back){
+rightIntake.setVelocity(100,pct);
+leftIntake.setVelocity(100,pct);
+if(Back != 0){
+  rightIntake.startRotateFor(Back, rev);
+  leftIntake.startRotateFor(Back, rev);
+}else if(Switch == 0){
+  rightIntake.spin(forward);
+  leftIntake.spin(forward);
+  Switch = 1;
+} else{
+  rightIntake.stop();
+  leftIntake.stop();
+  Switch = 0;
+}
+
+}
+
+void outake(float distance){
+
+rightIntake.startRotateFor(-distance,rev, 200,rpm);
+leftIntake.startRotateFor(-distance,rev, 200,rpm);
+
+}
+
+/*void rampo(int revso, float percentage){
+ramp.rotateTo(revso,percentage,pct)
+
+}
+*/
+
+void lowTower(){
+
+ramp.startRotateTo(3.5, rotationUnits::rev, 100, velocityUnits::pct);
+task::sleep(50);
+lift.startRotateTo(3, rotationUnits::rev, 100, velocityUnits::pct);
+
+}
+
+void popRamp(){
+
+intake(0);
+wait(500,msec);
+ramp.startRotateTo(2.5, rotationUnits::rev, 100, velocityUnits::pct);
+task::sleep(300);
+lift.rotateTo(2, rotationUnits::rev, 100, velocityUnits::pct);
+intake(0);
+lift.rotateTo(0, rotationUnits::rev, 100, velocityUnits::pct);
+
+}
